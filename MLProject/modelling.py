aaecd10560b87a3795ae782b8e8d2ea936210813
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2025-12-24 11:08:48 +0700

    feat: add parameter from tuning

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import fbeta_score
 import mlflow
 import utils
 import os
@@ -11,11 +12,25 @@ X_train, y_train, X_test, y_test = utils.load_dataset()
 with mlflow.start_run() as run:
     mlflow.set_experiment_tag("source", "github-action")
     
-    model = RandomForestClassifier(random_state=42, class_weight='balanced')
+    params = {
+            'n_estimators':300,
+            'max_depth': 10,
+            'min_samples_split': 7,
+            'max_features':  0.2700686061348578,
+            'min_samples_leaf': 4,
+            'class_weight': 'balanced',
+            'random_state': 42,
+            'n_jobs': -1
+    }
+    model = RandomForestClassifier(**params)
     model.fit(X_train, y_train)
 
     utils.log_metrics(model, "testing", X_test, y_test, class_metric=True)
     
+    y_pred = model.predict(X_test)
+    score = fbeta_score(y_test, y_pred, beta=2)
+    mlflow.log_metric("test_f2_score_rain", score)
+
     mlflow.sklearn.save_model(model, "model")
 
     run_id = run.info.run_id
