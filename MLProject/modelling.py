626f9b803acbd171a88fc23da1ff36d541e76fef
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2025-12-24 08:23:55 +0700

    feat: build model workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,14 +1,25 @@
 from sklearn.ensemble import RandomForestClassifier
 import mlflow
 import utils
+import os
 
 mlflow.sklearn.autolog()
 mlflow.set_experiment("random_forest")
 
 X_train, y_train, X_test, y_test = utils.load_dataset()
 
-with mlflow.start_run():
+with mlflow.start_run() as run:
+    mlflow.set_experiment_tag("source", "github-action")
+    
     model = RandomForestClassifier(random_state=42, class_weight='balanced')
     model.fit(X_train, y_train)
 
     utils.log_metrics(model, "testing", X_test, y_test, class_metric=True)
+    
+    mlflow.sklearn.save_model(model, "model")
+
+    run_id = run.info.run_id
+        
+    if "GITHUB_OUTPUT" in os.environ:
+        with open(os.environ["GITHUB_OUTPUT"], "a") as f:
+            print(f"mlflow_run_id={run_id}", file=f)
